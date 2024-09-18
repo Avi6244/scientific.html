6f51cd37ada6874c9c994d3673852a86d1b25ac7
Avi6244 <aavi3306@gmail.com>
2024-09-18 12:21:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*let backBtn = document.querySelector(".container .scientific-btns .back-btn");
+let backBtn = document.querySelector(".container .scientific-btns .back-btn");
 let scientificBtnBox = document.querySelector(".container .scientific-btns");
 let container = document.querySelector(".container");
 let input = document.querySelector(".container .input-box input");
@@ -46,40 +46,6 @@ backBtn.addEventListener("click",()=>{
 let clearInput =() =>{
     input.value = '';
     result.innerHTML = 0;
-}*/
-let result = document.querySelector('.container .result-box input');
-let btns = document.querySelectorAll('.container .btns-box .btn');
-let evalBtn = document.querySelector('.container .btns-box .eval-btn');
-let clrBtn = document.querySelector('.container .btns-box .clr-btn');
-let delBtn = document.querySelector('.container .btns-box .delete-btn');
-
-let getValue =(e)=>{
-  let btnText = e.target.innerHTML;
-  if(btnText == '×'){
-     btnText = '*'    
-  }
-  if(btnText == '÷'){
-     btnText = '/';   
-  }
-  result.value += btnText;
 }
 
-evalBtn.addEventListener('click',()=>{
- try{
-   result.value = eval(result.value);   
- }catch(err){
-   result.value = 'Error';    
- }    
-})
-
-clrBtn.addEventListener('click',()=>{
-  result.value = '';     
-});
-
-delBtn.addEventListener('click',()=>{
-  result.value = result.value.substr(0,result.value.length - 1);
-})
-
-for(let i=0;i<btns.length;i++){
-  btns[i].addEventListener('click',getValue)  
-}
+ 
